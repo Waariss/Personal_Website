e64a18cc305bf9adb2d6d4668d910a64ced88a44
Waariss <waris.dam@student.mahidol.ac.th>
2023-10-24 19:55:44 +0700

    update project

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -78,7 +78,7 @@ const Publication = ({links }) => {
 const getPublications = () => [
     {
         title: 'Detecting Vulnerable OAuth 2.0 Implementations in Android Applications',
-        subtitle: 'To be presented at the Workshop on Cyber Forensics, Security, and E-discovery, as part of the 23rd IEEE International Conference on Software Quality, Reliability, and Security, 2023.',
+        subtitle: 'Presented at the Workshop on Cyber Forensics, Security, and E-discovery, as part of the 23rd IEEE International Conference on Software Quality, Reliability, and Security, 2023.',
         description: 'OAuth 2.0, commonly used for authorization, can be susceptible to CSRF attacks in Android applications. To address this, we developed an Android app to assess other apps\' use of the OAuth 2.0 state parameter—a key defense against CSRF. Our analysis, conducted on both Chrome and the default browser, evaluates whether Android apps using OAuth 2.0 are adequately protected against CSRF attacks. Our research aims to protect users by highlighting apps with potentially vulnerable OAuth 2.0 implementations.', // Truncated for brevity
         link: 'https://github.com/Waariss/Detecting-Vulnerable-OAuth-2.0-Implementations-in-Android-Applications/tree/main'
     },
