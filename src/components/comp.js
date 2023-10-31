e15408f2dc164a8c48687eccc1f85e533c3c5d49
Waariss <waris.dam@student.mahidol.ac.th>
2023-11-01 00:12:38 +0700

    update COMP

--- src/components/comp.js
+++ src/components/comp.js
@@ -16,37 +16,6 @@ const Competitions = () => {
         slidesToScroll: 1
     };
 
-    // return (
-    //     <section id="competitions" className="my-5">
-    //         <h1 className="mb-4 text-center title-enhanced">Competitions</h1>
-    //         {getCompetitions().map((competition, index) => (
-    //             <Card key={index} className="mb-4 competition-card">
-    //                 <img src={competition.certificateImage} alt="Competition Certificate" className="certificate-image" />
-    //                 <Card.Body>
-    //                     <Card.Title className="competition-title">{competition.title}</Card.Title>
-    //                     <Card.Subtitle className="mb-2 text-muted">{competition.date}</Card.Subtitle>
-    //                     <Card.Text>{competition.description}</Card.Text>
-    //                     <Card.Text>
-    //                         <strong>Skills:</strong> <span className="skills">{competition.skills}</span>
-    //                     </Card.Text>
-    //                     <Card.Text className="link-group">
-    //                         {competition.links.map((link, linkIndex) => (
-    //                             <Card.Link
-    //                                 key={linkIndex}
-    //                                 href={link.url}
-    //                                 target="_blank"
-    //                                 rel="noopener noreferrer"
-    //                                 className="btn btn-outline-dark"
-    //                             >
-    //                                 {link.label}
-    //                             </Card.Link>
-    //                         ))}
-    //                     </Card.Text>
-    //                 </Card.Body>
-    //             </Card>
-    //         ))}
-    //     </section>
-    // );
     return (
         <section id="competitions" className="my-5">
             <h1 className="mb-4 text-center title-enhanced">Competitions</h1>
@@ -90,7 +59,7 @@ const getCompetitions = () => [
         title: 'Huntress CTF',
         date: 'Oct 2023',
         description: 'I participated in the Huntress Capture the Flag (CTF) Contest sponsored by Huntress Labs Incorporated. In this cybersecurity competition, participants solve challenges in domains like web security and cryptography to find and submit flags, earning points. The top scorers are declared winners.',
-        skills: 'Cybersecurity, Web Application Security, Digital Forensics, Reverse Engineering, Network Security, Mobile Security, Programming, Cryptography',
+        skills: 'Cybersecurity, Web Application Security, Malware, Steganography, OSINT, Miscellaneous',
         links: [
             { url: 'https://huntress.ctf.games', type: 'news', label: 'Website' },
         ]
