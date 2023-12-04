2cb90cb06d3f777321d80bdb85b147293c1f8d6b
Waariss <waris.dam@student.mahidol.ac.th>
2023-12-04 23:28:04 +0700

    remove use effect

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Card, Row, Col, Image, Button, OverlayTrigger, Tooltip} from 'react-bootstrap';
 import '../App.css';
 
